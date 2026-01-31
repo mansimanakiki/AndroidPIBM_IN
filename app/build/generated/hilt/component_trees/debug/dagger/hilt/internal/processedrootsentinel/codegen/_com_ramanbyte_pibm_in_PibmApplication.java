badd2febdbba1dad1b764799059763e26a4f9b9d
Mansi Mody <mansi.m@ramanbyte.com>
2026-01-31 12:01:25 +0530

    Fix .gitignore and remove build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_ramanbyte_pibm_in_PibmApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.ramanbyte.pibm_in.PibmApplication"
-)
-public final class _com_ramanbyte_pibm_in_PibmApplication {
-}
